587066b0104dd39ccf18ecac7aa593ef1afb2af6
Mahmoodreza Aarabi <madoodia@gmail.com>
2020-09-18 15:35:25 -0700

    learning but GLSL is enough

---
+++ GLSL/01.Sample1/script.js
@@ -0,0 +1,59 @@
+/* --------------------- */
+/* (C) 2020 madoodia.com */
+/*  All Rights Reserved. */
+/* --------------------- */
+
+// Define Vertex Shader
+const vShader = `
+  // GLSL Code Here
+`
+
+// Define Fragment Shader
+const fShader = `
+  // GLSL Code Here
+`
+
+
+// ---------------------------
+// Create Scene, Camera and Renderer
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create Contents
+const geometry = new THREE.PlaneGeometry(2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x464646 });
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
+camera.position.z = 1;
+
+onWindowResize();
+animate();
+
+// ---------------------------
+
+function onWindowResize(event) {
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  let width, height;
+  if (aspectRatio >= 1) {
+    width = 1;
+    height = (window.innerHeight / window.innerWidth) * width;
+  } else {
+    width = aspectRatio;
+    height = 1;
+  }
+  camera.left = -width;
+  camera.right = width;
+  camera.top = height;
+  camera.bottom = -height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
