a9a88434763540e1f8b68d0bca5579a85ddae230
Reza Aarabi <madoodia@gmail.com>
2020-10-18 20:15:20 -0700

    studying Truchet Tiling

---
+++ GLSL_Practices/Truchet_02/script.js
@@ -0,0 +1,125 @@
+/* --------------------- */
+/* (C) 2020 madoodia.com */
+/*  All Rights Reserved. */
+/* --------------------- */
+
+// Define Vertex Shader
+const vShader = `
+// These uniform variables are difined by default in threejs
+// layout(location=0)in vec3 position;
+// uniform mat4 projectionMatrix;
+// uniform mat4 viewMatrix;
+// uniform mat4 modelMatrix;
+
+// attribute vec3 position;
+// attribute vec3 normal;
+// attribute vec2 uv;
+
+void main()
+{
+  gl_Position = projectionMatrix * viewMatrix * modelMatrix * 
+                vec4(position * 0.3, 1.0);
+}
+`
+
+// Define Fragment Shader
+const fShader = `
+// Is defined by default
+// out vec4 gl_FragColor;
+
+uniform float uTime;
+uniform vec2 uResolution;
+
+float hash21(vec2 p)
+{
+  p = fract(p*vec2(234.34, 435.345));
+  p += dot(p, p+34.23);
+  return fract(p.x*p.y);
+}
+
+void main()
+{
+  vec2 uv = gl_FragCoord.xy/uResolution.y;
+  uv *= 20.0;
+
+  vec2 gv = fract(uv)-.5;
+  vec2 id = floor(uv);
+
+  vec3 color = vec3(0.0);
+
+  float n = hash21(id); // retuen random number between 0 and 1
+  float width = .1;
+
+  if(n<.5) gv.x *= -1.;
+  float mask = smoothstep(.01, -.01, abs(gv.x+gv.y) - width);
+
+  color += mask;
+  // color += n;
+
+  // if(gv.x>.48 || gv.y>.48) color = vec3(1, 0, 0); // boundries
+  gl_FragColor = vec4(color, 1.0);
+}
+`
+
+
+// ---------------------------
+// Create Scene, Camera and Renderer
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Custom Uniform attributes
+const uniforms = {
+  uTime: { value: 0.0 },
+  uResolution: { value: { x: 0.0, y: 0.0 } },
+}
+
+// Get Current time
+const clock = new THREE.Clock();
+
+// Create Contents
+const geometry = new THREE.PlaneGeometry(2, 2);
+const material = new THREE.ShaderMaterial({
+  uniforms: uniforms,
+  vertexShader: vShader,
+  fragmentShader: fShader
+});
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
+camera.position.z = 1;
+
+onWindowResize();
+animate();
+
+// ---------------------------
+
+function onWindowResize(event) {
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  let width, height;
+  if (aspectRatio >= 1) {
+    width = 1;
+    height = (window.innerHeight / window.innerWidth) * width;
+  } else {
+    width = aspectRatio;
+    height = 1;
+  }
+  camera.left = -width;
+  camera.right = width;
+  camera.top = height;
+  camera.bottom = -height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  if (uniforms.uResolution != undefined) {
+    uniforms.uResolution.value.x = window.innerWidth;
+    uniforms.uResolution.value.y = window.innerHeight;
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  uniforms.uTime.value = clock.getElapsedTime();
+}
